32dcf0209295e1051e0326fb03fda42e46b1a69a
maria2332 <maria.ballesteros2004@gmail.com>
2022-12-19 09:51:38 +0100

    cambio

---
+++ ejercicio8.py
@@ -0,0 +1,13 @@
+"""
+Estás de camino al mercado cuando escuchas una hermosa música de un artista callejero cercano. Las notas se juntan como si no lo creerías mientras el músico junta patrones de melodías. Mientras te preguntas qué tipo de algoritmo podrías usar para cambiar las octavas en 8 tonos o algo así de tonto, te das cuenta de que has estado observando al músico durante unos 10 minutos impares. Usted pregunta, "¿cuánto suele dar propina la gente por algo como esto?" El artista mira hacia arriba. "Siempre va a tratarse de Tree Fiddy".
+
+¡Fue entonces cuando te das cuenta de que el músico era una bestia de 400 pies de altura de la era paleolítica! ¡El Monstruo del Lago Ness casi te engaña!
+
+Solo hay 2 formas garantizadas de saber si estás hablando con el Monstruo del Lago Ness: A) es una bestia de 400 pies de altura de la era paleolítica; B) te pedirá árbol fiddy.
+
+Dado que Nessie es una maestra del disfraz, la única forma de saberlo con precisión es buscar la frase "tree fiddy". Como estás cansado de que este monstruo te engañe, ha llegado el momento de codificar una solución para encontrar al Monstruo del Lago Ness. Tenga en cuenta que la frase también se puede escribir como "3.50"o "three fifty".
+
+La mejor forma de detectar a Monstruo del Lago Ness es utilizando una expresión regular (re) para buscar la frase "tree fiddy" o sus variantes "3.50" y "three fifty" en una cadena de texto. Una vez que se encuentra una de estas frases, se puede determinar con certeza que se está hablando con el Monstruo del Lago Ness.
+
+Para ello, se puede usar el módulo re y su función search(), que buscará la primera coincidencia en la cadena de texto.
+"""
