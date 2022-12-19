c68b30a73e49caf2fbaaabda6f810753edb4cd5c
maria2332 <maria.ballesteros2004@gmail.com>
2022-12-19 10:56:04 +0100

    cambio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ejercicio1, ejercicio2, ejercicio3, ejercicio5, ejercicio7, ejercicio9, ejercicio10, ejercicio8, ejercicio4
 
-https://github.com/maria2332/Examen_19-12-22.git
+# https://github.com/maria2332/Examen_19-12-22.git
 
 
 
